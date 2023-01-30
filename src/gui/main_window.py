91d30bb39d0020787c9d95beeaa1fc637fb022cc
thisisjab <heythisisjab@gmail.com>
2023-01-30 21:57:21 +0330

    Create main window music pane

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,6 +1,7 @@
-from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QSizePolicy
+from PyQt6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QSizePolicy, QSplitter, QScrollBar
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QPixmap
+from .font_loader import load_fonts
 import logging
 
 
@@ -14,10 +15,11 @@ class MainWindow(QWidget):
         self.__configure_window()
         self.__load_styles()
         self.__add_widgets()
+        load_fonts()
 
     def __configure_window(self) -> None:
         self.setWindowTitle("Wavy")
-        self.setMinimumSize(500, 400)
+        self.setMinimumSize(750, 500)
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
         self.setAutoFillBackground(True)
 
@@ -38,7 +40,6 @@ class MainWindow(QWidget):
     def __add_widgets(self):
         self.widget_layout.addWidget(_LeftSidebar())
         self.widget_layout.addWidget(_CenterPanel())
-        self.widget_layout.addWidget(_RightSidebar())
 
 
 class _LeftSidebar(QWidget):
@@ -90,27 +91,97 @@ class _CenterPanel(QWidget):
     def __init__(self) -> None:
         super(_CenterPanel, self).__init__()
         self.__configure()
-        # self.__add_widgets()
+        self.__add_widgets()
 
     def __configure(self) -> None:
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
         self.setAutoFillBackground(True)
         self.setObjectName("center_panel")
-        self.widget_layout = QVBoxLayout()
+        self.widget_layout = QHBoxLayout()
+        self.widget_layout.setContentsMargins(0, 0, 0, 0)
+        self.widget_layout.setSpacing(0)
         self.setLayout(self.widget_layout)
 
+    def __add_widgets(self) -> None:
+        self.widget_layout.addWidget(_CenterPanelSplitter())
 
-class _RightSidebar(QWidget):
+
+class _CenterPanelSplitter(QSplitter):
 
     def __init__(self) -> None:
-        super(_RightSidebar, self).__init__()
+        super(_CenterPanelSplitter, self).__init__()
+
+        self.__add_widgets()
+
+    def __add_widgets(self) -> None:
+        self.addWidget(_SplitterMainPane())
+        self.addWidget(_SplitterMusicPane())
+
+
+class _SplitterMainPane(QWidget):
+
+    def __init__(self) -> None:
+        super(_SplitterMainPane, self).__init__()
+
         self.__configure()
 
     def __configure(self) -> None:
-        self.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
         self.setAutoFillBackground(True)
-        self.setObjectName("right_sidebar")
-        self.widget_layout = QVBoxLayout()
+        self.setObjectName("splitter_main_pane")
+        self.widget_layout = QHBoxLayout()
         self.setLayout(self.widget_layout)
+
+
+class _SplitterMusicPane(QWidget):
+
+    def __init__(self) -> None:
+        super(_SplitterMusicPane, self).__init__()
+
+        self.__configure()
+        self.__add_widgets()
+
+    def __configure(self) -> None:
+        self.setFixedWidth(300)
+        self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
+        self.setAutoFillBackground(True)
+        self.setObjectName("splitter_music_pane")
+        self.widget_layout = QVBoxLayout()
+        self.widget_layout.setContentsMargins(0, 0, 0, 0)
+        self.widget_layout.setSpacing(0)
+        self.widget_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.setLayout(self.widget_layout)
+
+    def __add_widgets(self) -> None:
+        # This is an empty label only to add margin from top
+        margin_label = QLabel()
+        margin_label.setContentsMargins(0,0,0,12)
+        self.widget_layout.addWidget(margin_label)
+
+        self.music_thumbnail_label = QLabel()
+        self.music_thumbnail_label.setFixedSize(250, 250)
+        self.music_thumbnail_label.setPixmap(QPixmap("static/image/app/album.png"))
+        self.music_thumbnail_label.setScaledContents(True)
+        self.music_thumbnail_label.setObjectName("music_pane_music_thumbnail_label")
+        self.widget_layout.addWidget(self.music_thumbnail_label)
+
+        self.music_title = QLabel("Hey you")
+        self.music_title.setWordWrap(True)
+        self.music_title.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.music_title.setObjectName("music_pane_music_title")
+        self.widget_layout.addWidget(self.music_title)
+
+        self.music_artist = QLabel("Pink Floyd")
+        self.music_artist.setWordWrap(True)
+        self.music_artist.setObjectName("music_pane_music_artist")
+        self.music_artist.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.widget_layout.addWidget(self.music_artist)
+
+        self.music_scrollbar = QScrollBar()
+        self.music_scrollbar.setFixedHeight(5)
+        self.music_scrollbar.setOrientation(Qt.Orientation.Horizontal)
+        self.music_scrollbar.setObjectName("music_pane_music_scroller")
+        self.widget_layout.addWidget(self.music_scrollbar)
+
+        self.widget_layout.addStretch()
